43411615cf2aca6734f74bf7f3a1ca54f7bd7a72
Richardalmeida21 <richard.c.dealmeida@gmail.com>
2024-09-09 14:53:29 -0300

    Att codigo prisma

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 
 const app = express();
 app.use(express.json());
 
-const users = []
+app.post('/users', async (req, res) => {
 
-app.post('/users', (req, res) => {
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.nome,
+            age: req.body.idade
+        }
+    })
 
-    users.push(req.body)
     res.status(201).json(req.body)
 
 })
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+
+   const users = await prisma.user.findMany()
+
     res.status(200).json(users)
 })
 
